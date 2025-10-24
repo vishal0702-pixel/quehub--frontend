667a8342204fd93240e08a6e6bfe07396a3dbe55
vishal k <vishalcodex07@gmail.com>
2025-10-25 05:10:40 +0530

    ad

--- src/utils/axiosclient.js
+++ src/utils/axiosclient.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const axiosclient = axios.create({
-  baseURL: 'https://quehub-backend.vercel.app/',
-  withCredentials:true,
-  headers: {'content-type': 'application/json'}
+  baseURL: "https://quehub-backend.vercel.app",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default axiosclient ;
+export default axiosclient;
